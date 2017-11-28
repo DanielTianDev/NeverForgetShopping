8a710ecea0d3ae3c0fdcfcb29fdde8c139c42f82
Daniel Tian <daniel_tian2002@hotmail.com>
2017-11-28 02:25:40 -0800

    added restfull connection

--- app/src/main/java/nier/neverforgetshopping/ShoppingListActivity.java
+++ app/src/main/java/nier/neverforgetshopping/ShoppingListActivity.java
@@ -2,14 +2,29 @@ package nier.neverforgetshopping;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+
+import java.io.*;
+import java.net.*;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 /**
  * Created by Kirito on 2017-11-23.
@@ -18,37 +33,184 @@ import java.util.Collections;
 public class ShoppingListActivity extends Activity{
 
 
-    ArrayList<String> shoppingList = null;
-    ArrayAdapter<String> adapter = null;
-    ListView lv = null;
+    public final static String SERVER_URL =  "http://danieltian.com/"; //"http://10.0.2.2:666/"; http://66.183.145.51
+
+    private ListView mShoppingList;
+    private EditText mItemEdit;
+    private Button mAddButton;
+    private ArrayAdapter<String> mAdapter;
+
+    private ArrayList<String> shoppingListItems = new ArrayList<String>();
+    private ArrayList<String> addedItems = new ArrayList<String>();
+
+    String teststr = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-        //startDateInput = (EditText) findViewById(R.id.startDateInput);
-        //endDateInput = (EditText) findViewById(R.id.endDateInput);
+
+        mShoppingList = (ListView) findViewById(R.id.shopping_listView);
+        mItemEdit = (EditText) findViewById(R.id.item_editText);
+        mAddButton = (Button) findViewById(R.id.add_button);
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        mShoppingList.setAdapter(mAdapter);
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = mItemEdit.getText().toString();
+                mAdapter.add(item);
+                mAdapter.notifyDataSetChanged();
+                mItemEdit.setText("");
+
+                try{
+                    new AddItemTask().execute(item);
+                }catch (Exception e){
+                    Log.e(MainActivity.DEBUG_DEFAULT_TAG, e.getMessage());
+                }
+
+
+
+            }
+        });
+
+        try{
+            new RetrieveFeedTask().execute(SERVER_URL+"api.php");
+           // Log.e(MainActivity.DEBUG_DEFAULT_TAG, "test msg: " + teststr);
+        }catch (Exception e){
+            Log.e(MainActivity.DEBUG_DEFAULT_TAG, e.getMessage());
+        }
+
+        Timer timer = new Timer();
+        timer.schedule(new RetrieveAllItems(), 0, 2500);
+
     }
 
 
-    void listsomething(){
-
-        shoppingList = new ArrayList<>();
-        Collections.addAll(shoppingList, "Eggs", "Yogurt", "Milk", "Bananas", "Apples", "Tide with bleach", "Cascade");
-        shoppingList.addAll(Arrays.asList("Napkins", "Dog Food", "Chap-stick", "Bread"));
-        shoppingList.add("Sunscreen");
-        shoppingList.add("Toothpaste");
-
-        //adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, shoppingList);
-
-        //lv = (ListView) findViewById(R.id.listView);
-
-        lv.setAdapter(adapter);
-
-        
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
 
 
+    class RetrieveFeedTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... urls) {
+            try {
+
+
+
+                teststr =  getHTML(SERVER_URL + "api.php");
+               // Log.e(MainActivity.DEBUG_DEFAULT_TAG, "doing in background " + teststr  );
+
+
+
+                String[] listItems = teststr.split("&&&");
+
+
+                shoppingListItems.clear();
+
+                for(int i=0;i<listItems.length;i++){
+                    shoppingListItems.add(listItems[i]);
+
+                }
+
+
+                for(int i=0;i<shoppingListItems.size();i++){
+                    if(!checkIfAdapterContains(shoppingListItems.get(i))){
+                        mAdapter.add(shoppingListItems.get(i));
+                        mAdapter.notifyDataSetChanged();
+                        mItemEdit.setText("");
+                    }
+
+
+                }
+
+                return teststr;
+
+            } catch (Exception e) {
+                this.exception = e;
+                return "exception";
+            }
+        }
+
+        protected void onPostExecute() {
+
+
+            Log.e(MainActivity.DEBUG_DEFAULT_TAG, "on post executed");
+
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+    }
+
+    class AddItemTask extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+
+        protected String doInBackground(String... itemName) {
+            try {
+
+
+
+                getHTML(SERVER_URL+"insertItem.php?itemName=" + itemName[0]);
+
+                return teststr;
+
+            } catch (Exception e) {
+                this.exception = e;
+                return "exception";
+            }
+        }
+
+        protected void onPostExecute() {
+
+
+            //Log.e(MainActivity.DEBUG_DEFAULT_TAG, "on post executed");
+
+            // TODO: check this.exception
+            // TODO: do something with the feed
+        }
+    }
+
+    class RetrieveAllItems extends TimerTask {
+        public void run() {
+
+            try{
+                new RetrieveFeedTask().execute(SERVER_URL+"api.php");
+                //ViewGroup vg = findViewById (R.id.shopping_listView);
+                //vg.invalidate();
+                //Log.e(MainActivity.DEBUG_DEFAULT_TAG, "test msg: " + teststr);
+            }catch (Exception e){
+                Log.e(MainActivity.DEBUG_DEFAULT_TAG, e.getMessage());
+            }
+
+
+        }
+    }
+
+    boolean checkIfAdapterContains(String value){
+        for (int i = 0; i < mShoppingList.getAdapter().getCount(); i++){
+            if(value.equals((String)mShoppingList.getAdapter().getItem(i))){
+                return true;
+            }
+        }
+
+
+        return false;
     }
 
 }
